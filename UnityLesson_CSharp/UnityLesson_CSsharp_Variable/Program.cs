85cb378925ef86c39d09f55b1e9f1e241c9f5c24
flexinja99 <98201760+flexinja99@users.noreply.github.com>
2022-01-29 19:09:20 +0900

    함수,oeprator

--- UnityLesson_CSharp/UnityLesson_CSsharp_Variable/Program.cs
+++ UnityLesson_CSharp/UnityLesson_CSsharp_Variable/Program.cs
@@ -18,8 +18,8 @@ namespace UnityLesson_CSsharp_Variable
         // 변수 선언 형태 : 타입(자료형) 변수이름(대소문자 구분 함)
         //
         // person 클래스의 맴버 변수 (필드)
-        int age; // 정수형 나이 // int : 4byte 의 정수 -2147483648~2147483647
-        float heigjt; // 실수형 키 // float: 4byte 의 실수
+        int age = 50; // 정수형 나이 // int : 4byte 의 정수 -2147483648~2147483647
+        float heigjt = 223.0f; // 실수형 키 // float: 4byte 의 실수
         bool isResting; //  논리형 쉬고있는가? // bool : 1 byte 논리 ( 참,거짓은 1bit 로 판별 가능하지만, CPU 의 데이터 처리 최소단위가 1byte 이므로 그 이하 단위 처리는 불가능하다)
         char genderChar; // 문자형 성별 문자 // char : 2byte 문자 
         string name;// 문자열형 이름 // string : 문자갯수 * 2byte 문자열
