501ae2cc5d4f3eadaab7daa5c21881fd129054b0
flexinja99 <98201760+flexinja99@users.noreply.github.com>
2022-02-12 18:48:26 +0900

    202202012
    stats

---
+++ UnityLesson_CSharp/UnityLesson_CSharp Equipment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityLesson_CSharp_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
